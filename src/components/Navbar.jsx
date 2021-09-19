92449b718af751335201f15efae9e0d13e86d9e7
Rian Oktio Mersa Putra <rianoktiomersaputra4@gmail.com>
2021-09-19 13:16:10 +0700

    feat(navbar): adding navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="navbar">
+        <h1>The Fue Blog</h1>
+        <div className="links">
+          <a href="/">Home</a>
+          <a href="/create">New Blog</a>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;

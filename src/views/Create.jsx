994d90cafb6fd6dfbdb3d0a1aee51f5d89f73835
Rian Oktio Mersa Putra <rianoktiomersaputra4@gmail.com>
2021-09-20 23:14:37 +0700

    feat(create post): programmatic post request

--- src/views/Create.jsx
+++ src/views/Create.jsx
@@ -1,12 +1,33 @@
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const Create = () => {
   const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("mario");
+  const [loading, setLoading] = useState(false);
+  const history = useHistory();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+
+    setLoading(true);
+
+    fetch("http://localhost:8000/blogs", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog),
+    }).then(() => {
+      setLoading(false);
+      history.push("/");
+    });
+  };
 
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
           Blog title: <span>{title}</span>
         </label>
@@ -16,14 +37,17 @@ const Create = () => {
           value={title}
           onChange={(e) => setTitle(e.target.value)}
         />
-        <label>Blog body:</label>
-        <textarea required></textarea>
-        <label>Blog author:</label>
-        <select>
+        <label>Blog body</label>
+        <textarea required onChange={(e) => setBody(e.target.value)}></textarea>
+        <label>
+          Blog author: <span>{author}</span>
+        </label>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
           <option value="mario">Mario</option>
           <option value="yoshi">Yoshi</option>
         </select>
-        <button>Add Blog</button>
+        {!loading && <button>Add Blog</button>}
+        {loading && <button disabled>Adding Blog ...</button>}
       </form>
     </div>
   );

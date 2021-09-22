84ceaeabc1c79378a0cabf8c3391157dbcfeebf6
Rian Oktio Mersa Putra <rianoktiomersaputra4@gmail.com>
2021-09-22 14:12:16 +0700

    feat(delete blog): adding delete blog so blog can be deleted

--- src/views/Blogs.jsx
+++ src/views/Blogs.jsx
@@ -1,13 +1,22 @@
-import { useParams } from "react-router";
+import { useHistory, useParams } from "react-router";
 import useFetch from "../hooks/useFetch";
 
 const Blogs = () => {
+  const history = useHistory();
   const { id } = useParams();
   const {
     data: blog,
     isLoading,
     messageError,
   } = useFetch(`http://localhost:8000/blogs/${id}`);
+
+  const handleDelete = () => {
+    fetch(`http://localhost:8000/blogs/${id}`, {
+      method: "DELETE",
+    }).then(() => {
+      history.push("/");
+    });
+  };
 
   return (
     <div className="blog-details">
@@ -18,6 +27,7 @@ const Blogs = () => {
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
           <div>{blog.body}</div>
+          <button onClick={handleDelete}>Delete</button>
         </article>
       )}
     </div>
